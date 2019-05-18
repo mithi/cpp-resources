2ff419f4d17f20786ce45358d9b6820ac5e02921
Mithi Sevilla <mithi.sevilla@gmail.com>
2019-05-18 18:52:11 +0800

    Remove unnecessary condition

--- exercises/exercise-1.cpp
+++ exercises/exercise-1.cpp
@@ -18,8 +18,8 @@ public:
 
 
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        if (l1 == NULL && l2 != NULL) { return l2; }
-        if (l2 == NULL && l1 != NULL) { return l1; }
+        if (l1 == NULL) { return l2; }
+        if (l2 == NULL) { return l1; }
 
         ListNode* sum = new ListNode(0);
         ListNode* s = sum;
